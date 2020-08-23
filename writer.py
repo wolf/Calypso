1b106392338e7f87009dfe6ce096a24591e35792
Wolf <wolf@zv.cx>
2020-08-22 21:32:45 -0400

    Make sure output files end with a newline

--- writer.py
+++ writer.py
@@ -2,3 +2,5 @@ def write_code_files(code_files):
     for file_name, code in code_files.items():
         with open(file_name, "w") as f:
             f.write(code)
+            if not code.endswith("\n"):
+                f.write("\n")
