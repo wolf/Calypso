12ab9ca9efccba89099c4e3703942bd2f0f00dff
Wolf <wolf@zv.cx>
2020-08-24 15:39:04 -0400

    For an output file, create the dir structure if requested

--- code_writer/code_writer.py
+++ code_writer/code_writer.py
@@ -1,4 +1,11 @@
+import pathlib
+
+
 def write_code_files(code_files):
     for file_name, code in code_files.items():
-        with open(file_name, "w") as f:
+        path = pathlib.Path(file_name)
+        owning_directory = path.parent
+        if not owning_directory.exists():
+            owning_directory.mkdir(parents=True)
+        with open(path, "w") as f:
             f.write(code)
