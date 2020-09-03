2a3746639e7cabcd943a656fd6fecfd6810c5fa9
Wolf <wolf@zv.cx>
2020-09-03 08:25:57 -0400

    Move core command; add weave subcommand; fix imports

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # To Do...
 
 * Use Click to make tangle and weave sub-commands of some outer command
+* Add logging
 * Integrate `pluggy` to get plug-in functionality
 * Write the db-based scanner
 * Write the HTML-writing emitter for code sections in weave mode
--- calypso.py
+++ calypso.py
@@ -1,20 +1,26 @@
 from pathlib import Path
 
-from base import CodeReaderError
-from bootstrap.code_reader import get_code_files
-from bootstrap.code_writer import write_code_files
-
 import click
 
+from calypso.base import CodeReaderError
+from calypso.bootstrap.code_reader import get_code_files
+from calypso.bootstrap.code_writer import write_code_files
 
-@click.command()
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
 @click.option("--output", "-o", type=str, multiple=True)
 @click.option("--base-dir", type=click.Path(file_okay=False, dir_okay=True, writable=True, readable=True))
 @click.argument(
     "file_paths", type=click.Path(exists=True, file_okay=True, dir_okay=False, writable=False, readable=True), nargs=-1,
 )
 def tangle(output, file_paths, base_dir):
-    roots_to_extract = set(output)
+    click.echo("tangling")
+    roots_to_extract = set(output) or None
     for file_path in file_paths:
         try:
             code_files = get_code_files(Path(file_path))
@@ -23,5 +29,10 @@ def tangle(output, file_paths, base_dir):
             print(f'Error while processing "{file_path}": {e.message}')
 
 
+@cli.command()
+def weave():
+    click.echo("weaving")
+
+
 if __name__ == "__main__":
-    tangle()
+    cli()
