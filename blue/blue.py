55f8f02812262cd3717766211f57f278b134839e
Wolf <wolf@zv.cx>
2020-09-06 16:59:37 -0400

    Make the tool call the new scanner version instead of the old

--- blue/blue.py
+++ blue/blue.py
@@ -3,8 +3,8 @@ from pathlib import Path
 import click
 
 from blue.base.exceptions import CodeReaderError
-from blue.bootstrap.code_reader import get_code_files
 from blue.bootstrap.code_writer import write_code_files
+from blue.bootstrap import scanner
 
 @click.group()
 @click.option("--debug/--no-debug", default=False)
@@ -14,23 +14,26 @@ def blue(ctx, debug, verbose):
     ctx.ensure_object(dict)
     ctx.obj["DEBUG"] = debug
     ctx.obj["VERBOSE"] = verbose
+    # TO DO: set up logging here
 
 
 @blue.command()
-@click.option("--output", "-o", type=str, multiple=True)
+@click.option("--extract-only", "-x", type=str, multiple=True)
 @click.option("--base-dir", type=click.Path(file_okay=False, dir_okay=True, writable=True, readable=True))
+@click.option("--database", type=str, default=":memory:")
 @click.argument(
     "file_paths", type=click.Path(exists=True, file_okay=True, dir_okay=False, writable=False, readable=True), nargs=-1,
 )
 @click.pass_context
-def tangle(ctx, output, file_paths, base_dir):
+def tangle(ctx, output, file_paths, base_dir, database):
     if ctx.obj["VERBOSE"]:
         message = "tangling with debug turned on" if ctx.obj["DEBUG"] else "tangling"
         click.echo(message)
     roots_to_extract = set(output) or None
     for file_path in file_paths:
         try:
-            code_files = get_code_files(ctx, Path(file_path))
+            scanner.parse_source_file(ctx, database, Path(file_path))
+            code_files = scanner.get_code_files(ctx)
             write_code_files(ctx, code_files, roots_to_extract, base_dir)
         except CodeReaderError as e:
             print(f'Error while processing "{file_path}": {e.message}')
