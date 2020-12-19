2ca4ed3d22ab37cfa7fd06f9ad500500cdfcb946
Wolf <wolf@zv.cx>
2020-12-19 10:38:13 -0500

    Use foreign keys where needed

--- blue/blue-schema.sql
+++ blue/blue-schema.sql
@@ -1,3 +1,6 @@
+PRAGMA foreign_keys = ON;
+
+
 DROP TABLE IF EXISTS document_section_kind;
 CREATE TABLE document_section_kind (
     id INTEGER PRIMARY KEY NOT NULL,
@@ -11,13 +14,15 @@ INSERT INTO document_section_kind (description) VALUES
 
 DROP TABLE IF EXISTS document_section;
 CREATE TABLE document_section (
-   id INTEGER PRIMARY KEY NOT NULL,
-   kind_id INTEGER NOT NULL,
-   is_included INTEGER, -- 0 or 1 (because there is no BOOLEAN type)
-   code_section_presentation_number INTEGER, -- will be NULL for kinds other than 'code'
-   sequence REAL,
-   name TEXT COLLATE NOCASE, -- will be NULL for kinds other than code
-   data TEXT
+    id INTEGER PRIMARY KEY NOT NULL,
+    kind_id INTEGER NOT NULL,
+    is_included INTEGER, -- 0 or 1 (because there is no BOOLEAN type)
+    code_section_presentation_number INTEGER, -- will be NULL for kinds other than 'code'
+    sequence REAL,
+    name TEXT COLLATE NOCASE, -- will be NULL for kinds other than code
+    data TEXT,
+
+    FOREIGN KEY (kind_id) REFERENCES document_section_kind(id)
 );
 
 
@@ -34,13 +39,17 @@ INSERT INTO fragment_kind (description) VALUES
 
 DROP TABLE IF EXISTS fragment;
 CREATE TABLE fragment (
-   id INTEGER PRIMARY KEY NOT NULL,
-   kind_id INTEGER NOT NULL,
-   parent_id INTEGER NOT NULL,
-   parent_name_id INTEGER,
-   sequence REAL,
-   data TEXT,
-   indent TEXT
+    id INTEGER PRIMARY KEY NOT NULL,
+    kind_id INTEGER NOT NULL,
+    parent_id INTEGER NOT NULL,
+    parent_name_id INTEGER,
+    sequence REAL,
+    data TEXT,
+    indent TEXT,
+
+    FOREIGN KEY (kind_id) REFERENCES fragment_kind(id),
+    FOREIGN KEY (parent_id) REFERENCES document_section(id),
+    FOREIGN KEY (parent_name_id) REFERENCES code_section_name(id)
 );
 
 
@@ -53,7 +62,9 @@ CREATE TABLE code_section_name (
 
 DROP TABLE IF EXISTS non_root_code_section_name;
 CREATE TABLE non_root_code_section_name (
-    name_id INTEGER NOT NULL UNIQUE
+    name_id INTEGER NOT NULL UNIQUE,
+
+    FOREIGN KEY (name_id) REFERENCES code_section_name(id)
 );
 
 
